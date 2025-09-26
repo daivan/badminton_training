6976482c8bf04ede17481352d381efcfb506cbb6
daivan <daivan@gmail.com>
2025-09-27 00:01:16 +0200

    vue

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,23 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/', // Replace with your GitHub repository name
+  base: './', // Set base to relative path for GitHub Pages
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  build: {
+    outDir: 'docs', // Output to the docs directory for GitHub Pages
+    assetsInlineLimit: 1000000, // Inline all assets
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true, // Inline dynamic imports
+        entryFileNames: 'assets/[name].js', // Keep original entry file names
+        chunkFileNames: 'assets/[name].js', // Keep original chunk file names
+        assetFileNames: 'assets/[name].[ext]', // Keep original asset file names
+      },
+    },
+    cssCodeSplit: false, // Ensure CSS is inlined
   },
 })
