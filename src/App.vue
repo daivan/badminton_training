c7b33d3db95cc63c36ad51ef4daa8bad892e44c0
daivan <daivan@gmail.com>
2025-09-26 23:44:56 +0200

    Vite

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import TrainingSelection from './components/TrainingSelection.vue';
+import TrainingExercise from './components/TrainingExercise.vue';
+
+const trainingStarted = ref(false);
+const trainingDuration = ref(0);
+
+const startTraining = (duration) => {
+  trainingDuration.value = duration;
+  trainingStarted.value = true;
+};
+
+const endTraining = () => {
+  trainingStarted.value = false;
+  trainingDuration.value = 0;
+};
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <TrainingSelection v-if="!trainingStarted" @start-training="startTraining" />
+    <TrainingExercise v-else :duration="trainingDuration" @end-training="endTraining" />
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+</style>
