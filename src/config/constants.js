8153afe30b364d5741a8ba832181f2dd59bbb7b8
daivan <daivan@gmail.com>
2026-03-26 16:26:07 +0100

    Fixed images hopefully

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,10 @@
+const asset = (path) => `${import.meta.env.BASE_URL}${path.replace(/^\/+/, '')}`;
+
 export const BADMINTON_SHOTS = [
-  { id: 'short_forehand', label: 'Short Forehand', src: '/images/badminton_shots/short_forehand.png' },
-  { id: 'short_backhand', label: 'Short Backhand', src: '/images/badminton_shots/short_backhand.png' },
-  { id: 'late_forehand', label: 'Late Forehand', src: '/images/badminton_shots/late_forehand.png' },
-  { id: 'late_backhand', label: 'Late Backhand', src: '/images/badminton_shots/late_backhand.png' },
+  { id: 'short_forehand', label: 'Short Forehand', src: asset('images/badminton_shots/short_forehand.png') },
+  { id: 'short_backhand', label: 'Short Backhand', src: asset('images/badminton_shots/short_backhand.png') },
+  { id: 'late_forehand', label: 'Late Forehand', src: asset('images/badminton_shots/late_forehand.png') },
+  { id: 'late_backhand', label: 'Late Backhand', src: asset('images/badminton_shots/late_backhand.png') },
 ];
 
 export const CALLS_TO_ACTION = [
